1560f2233f0c3574fd0fd15e9f874dce7e0bcdc0
sortman <ortman@alumni.nd.edu>
2013-07-07 00:35:05 +0000

    added chaged web

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(16);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    response.send(fs.readFileSync('index.html', 'utf-8');
 });
 
 var port = process.env.PORT || 5000;
