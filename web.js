7f29f22b13fa640508a7ec846daa06c6222254ba
sortman <ortman@alumni.nd.edu>
2013-07-07 00:42:19 +0000

    changed web

--- web.js
+++ web.js
@@ -2,14 +2,15 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(16);
+var fs = require('fs');
 
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
-    response.send(fs.readFileSync('index.html', 'utf-8');
+    response.send(fs.readFileSync('index.html', 'utf-8'));
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  console.log(fs.readFileSync('index.html', 'utf-8'));
 });
